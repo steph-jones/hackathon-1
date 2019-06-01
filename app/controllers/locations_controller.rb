e0dca67c1d56ad1daf5c6f2891e5c6d0d38f0535
Aaron Krishnan <buzzkri@gmail.com>
2019-06-01 16:04:36 -0700

    1.6

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -48,7 +48,7 @@ class LocationsController < ApplicationController
   end
 
   def set_location
-    @location = Location.find(params[:trip_id])
+    @location = Location.find(params[:id])
   end
 
   def location_params
